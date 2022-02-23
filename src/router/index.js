ab7a84e87e37fc78ca3c4a89ea5dabb22d1a2d2d
邵励治 <shaolizhi1234@126.com>
2022-02-23 20:03:25 +0800

    初始化了代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,17 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import VideoEditor from "../views/VideoEditor.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/",
+    name: "viedoeditor",
+    component: VideoEditor
   }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
-})
+});
 
-export default router
+export default router;
