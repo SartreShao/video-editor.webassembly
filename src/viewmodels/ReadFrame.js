368edf412fdd5714a368a282bc9115b0b2e65c0c
邵励治 <shaolizhi1234@126.com>
2022-03-23 14:28:46 +0800

    add ReadFrame.renderFramesMap

--- src/viewmodels/ReadFrame.js
+++ src/viewmodels/ReadFrame.js
@@ -35,6 +35,106 @@ const excuteReadFrameTask = (
   }
 };
 
+const renderFramesMap = (oldFramesMap, flatFrameList, videoFrameBuffer) => {
+  // crete newFramesMap
+  const newFramesMap = createFramesMap(
+    flatFrameList.value,
+    videoFrameBuffer.value
+  );
+
+  updateFramesMap(oldFramesMap.value, newFramesMap);
+
+  const createFramesMap = (flatFrameList, videoFrameBuffer) => {
+    const newFramesMap = new Map();
+
+    for (let i = 0; i < flatFrameList.length; i++) {
+      const flatFrame = flatFrameList[i];
+
+      // construct FramesMap's Item: key is videoIndex which from flatFrame
+      const currentFramesMapItem = newFramesMap.has(flatFrame.videoIndex)
+        ? newFramesMap.get(flatFrame.videoIndex)
+        : new Map();
+
+      // get blobUrl from videoFrameBuffer: key is { videoIndex, frame } which from flatFrame
+      const videoFrameBufferKey = JSON.stringify({
+        videoIndex: flatFrame.videoIndex,
+        frame: flatFrame.frame
+      });
+
+      const blobUrl = videoFrameBuffer.has(videoFrameBufferKey)
+        ? videoFrameBuffer.get(videoFrameBufferKey)
+        : "";
+
+      // construct newFramesMap
+      currentFramesMapItem.set(flatFrame.frame, {
+        blobUrl: blobUrl,
+        position: flatFrame.position
+      });
+
+      newFramesMap.set(flatFrame.videoIndex, currentFramesMapItem);
+    }
+
+    return newFramesMap;
+  };
+
+  const updateFramesMap = (oldFramesMap, newFramesMap) => {
+    // 新无旧有，则旧删
+    oldFramesMap.forEach((value, key) => {
+      if (!newFramesMap.has(key)) {
+        oldFramesMap.delete(key);
+      }
+    });
+
+    // 新有旧无，则旧增
+    newFramesMap.forEach((value, key) => {
+      if (!oldFramesMap.has(key)) {
+        oldFramesMap.set(key, value);
+      }
+    });
+
+    // 增量更新子节点
+    oldFramesMap.forEach((value, key) => {
+      updateChildMap(oldFramesMap.get(key), newFramesMap.get(key));
+    });
+
+    const updateChildMap = (oldChildMap, newChildMap) => {
+      // 新无旧有，则旧删
+      oldChildMap.forEach((value, key) => {
+        if (!newChildMap.has(key)) {
+          oldChildMap.delete(key);
+        }
+      });
+
+      // 新有旧无，则旧增
+      newChildMap.forEach((value, key) => {
+        if (!oldChildMap.has(key)) {
+          oldChildMap.set(key, value);
+        }
+      });
+
+      // 遍历旧节点，如果 value 不对，则更新 value
+      oldChildMap.forEach((value, key) => {
+        if (!isBlobUrlSame(oldChildMap.get(key), newChildMap.get(key))) {
+          oldChildMap.get(key).blobUrl = newChildMap.get(key).blobUrl;
+        }
+
+        if (!isPositionSame(oldChildMap.get(key), newChildMap.get(key))) {
+          oldChildMap.get(key).position = newChildMap.get(key).position;
+        }
+      });
+
+      const isBlobUrlSame = (oldVal, newVal) => {
+        return oldVal.blobUrl === newVal.blobUrl;
+      };
+
+      const isPositionSame = (oldVal, newVal) => {
+        return oldVal.position === newVal.position;
+      };
+    };
+  };
+};
+
 export default {
-  excuteReadFrameTask
+  excuteReadFrameTask,
+  renderFramesMap
 };
